ae903c21fc11c206c9349123efe91350aeaf7713
Spiro Andreakis <ernesto.andreakis@directenergy.com>
2015-08-21 13:52:33 -0500

    Added composer package definition. More filtering types using the purify library

--- src/Filter.php
+++ src/Filter.php
@@ -7,6 +7,9 @@
 
 namespace DE\CSFilter;
 
+use \HTMLPurifier;
+use \HTMLPurifier_Config;
+
 /**
  * Class Filter
  * @package DE\CSFilter
@@ -14,10 +17,24 @@ namespace DE\CSFilter;
 class Filter extends SingletonAbstract implements FilterInterface
 {
     /**
+     * Defines the value for the default charset
+     */
+    const DEFAULT_CHARSET = 'UTF-8';
+    /**
+     * Array of configs needed for purifying some variables with the purifier library
+     * @var array
+     */
+    protected static $configs = [];
+    /**
+     * An instance of the purifiers library to be used
+     * @var object
+     */
+    protected static $purifierLib;
+    /**
      * An array with the allowed options for the filters
      * @var array
      */
-    protected $allowedFilters = [
+    protected static $allowedFilters = [
         self::TYPE_BOOLEAN,
         self::TYPE_EMAIL,
         self::TYPE_INTEGER,
@@ -47,8 +64,12 @@ class Filter extends SingletonAbstract implements FilterInterface
         return self::$instance;
     }
 
+    /**
+     * Return the array of allowed filters
+     * @return array
+     */
     public static function getAllowedFilters() {
-
+        return self::$allowedFilters;
     }
 
     /**
@@ -96,6 +117,73 @@ class Filter extends SingletonAbstract implements FilterInterface
      */
     public static function filterEmail($dirtyVar) {
         return filter_var($dirtyVar, FILTER_SANITIZE_EMAIL);
+    }
+
+    /**
+     * Cleans a variable according to type and settings by using a third party library
+     * @param string $dirtyValue The value to be cleaned
+     * @param string $filterType What type of filter to apply
+     * @param array $optionsForPurifierLib Assoc array of options for the library conf object
+     * @return string The clean value
+     * @throws Exception
+     */
+    protected static function clean($dirtyValue, $filterType, array $optionsForPurifierLib = []) {
+        if(in_array($filterType, self::$allowedFilters)) {
+            $configHash = md5($filterType . json_encode($optionsForPurifierLib));
+            if(isset(self::$configs[$configHash])) {
+                $configObj = self::$configs[$configHash];
+            }
+            else {
+                $configObj = HTMLPurifier_Config::createDefault();
+                foreach($optionsForPurifierLib as $option => $value) {
+                    $configObj->set($option,$value);
+                }
+                self::$configs[$configHash] = $configObj;
+            }
+            $purifierLib = new HTMLPurifier($configObj);
+            $purifiedValue = $purifierLib->purify($dirtyValue);
+        }
+        else {
+            $validTypes = implode('\',\'', self::$allowedFilters);
+            throw new Exception("Filter type provided is not valid. Got: '{$filterType}'. Expecting one of: '{$validTypes}'");
+        }
+        return $purifiedValue;
+    }
+
+    /**
+     * Cleans a string
+     * @param mixed $dirtyVar The value to be cleansed
+     * @param array $options Additional options defining the charset to be used as array('charset' => 'ISO8859-1'). Defaults to UTF-8 [OPTIONAL]
+     * @return string
+     * @throws Exception
+     */
+    public static function filterString($dirtyVar, array $options = []) {
+        $charset = isset($options['charset'])?$options['charset']:self::DEFAULT_CHARSET;
+        $configObjOptions = [
+            'Core.Encoding' => $charset,
+            'HTML.Allowed' => '',
+        ];
+        return self::clean($dirtyVar, self::TYPE_STRING, $configObjOptions);
+    }
+
+    /**
+     * Cleans a string allowing certain tags (p, a[href|title],abbr[title],acronym[title],b,strong,blockquote[cite],code,em,i,strike)
+     * @param string $dirtyVar The dirty string
+     * @param array $options Additional options defining the charset to be used as array('charset' => 'ISO8859-1'). Defaults to UTF-8 [OPTIONAL]
+     * @return string The cleansed string
+     * @throws Exception
+     */
+    public static function filterRich($dirtyVar, array $options = []) {
+        $charset = isset($options['charset'])?$options['charset']:self::DEFAULT_CHARSET;
+        $configObjOptions = [
+            'Core.Encoding' => $charset,
+            'HTML.Doctype' => 'XHTML 1.0 Strict',
+            'HTML.Allowed' => 'p,a[href|title],abbr[title],acronym[title],b,strong,blockquote[cite],code,em,i,strike',
+            'AutoFormat.AutoParagraph' => true,
+            'AutoFormat.Linkify' => true,
+            'AutoFormat.RemoveEmpty' => true,
+        ];
+        return self::clean($dirtyVar, self::TYPE_RICH, $configObjOptions);
     }
 
     /**
